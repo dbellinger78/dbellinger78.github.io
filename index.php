5703f6bdbcdffa988223f0eda9779724bd9402a7
Dustin Bellinger <dustin.bellinger@mail.com>
2021-08-25 21:35:26 -0500

    Added initial files and images for site build

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to Cardboard Conservation</title>
+ h1{
+	color: white
+	background-color: black
+	font-family: san-serief
+ }
+ 
+
+
+</head>
+
+<body>
+
+<h1>Welcome to Cardboard Conservation!</h1>
+
+</body>
+
+</html>
